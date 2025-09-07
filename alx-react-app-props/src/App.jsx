2fe45800ea949931c7a98142633c9c127fbd3eab
sideeqhammed <sideeqhammed05.com>
2025-09-07 18:03:40 +0100

    Created alx-react-app-props folder

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import UserContext from './contexts/UserContext';
+import ProfilePage from './ProfilePage';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  )
+}
+
+export default App;
