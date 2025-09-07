ad984e7686ae2517e3d50ec9cc7f29134d97644d
sideeqhammed <sideeqhammed05.com>
2025-09-07 16:48:07 +0100

    Added alx-react-app-new folder which is a clone of alx-react-app and some inline styles

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 export default function UserProfile (props) {
   return (
-    <div style={{backgroundColor: 'gray', border: 'solid', borderRadius: '10px', width: '30em', margin: 'auto'}}>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
+    <div style={{backgroundColor: 'gray', padding: '10px', border: 'solid', borderRadius: '10px', width: '30em', margin: 'auto'}}>
+      <h2 style={{color: 'blue'}}>{props.name}</h2>
+      <p>Age: <span style={{fontWeight: 'bold'}}>{props.age}</span></p>
       <p>Bio: {props.bio}</p>
     </div>
   )
