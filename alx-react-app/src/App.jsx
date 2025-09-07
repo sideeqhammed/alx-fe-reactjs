29c78b38f5c02f4e6aa0438f7769caafb44874c2
sideeqhammed <sideeqhammed05.com>
2025-09-07 16:43:28 +0100

    Added userprofile.jsx and imported it to app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile />
       <Footer />
     </>
   )
