8969c2b15573270038001995fba20855600883dc
sideeqhammed <sideeqhammed05.com>
2025-09-07 16:31:48 +0100

    Revert "Added userprofile.jsx and imported it to app.jsx"
    This reverts commit c76d2ab3c7fd3668f805bb2569e45a5346006b7f.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,7 +6,6 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +15,6 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   )
--- alx-react-app/src/components/UserProfile.jsx
+++
@@ -1,9 +0,0 @@
-export default function UserProfile (props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  )
-}
