945a48ef4c6df3e7dd955a10ebfda60c1b4c39f9
sideeqhammed <sideeqhammed05.com>
2025-09-15 08:24:52 +0100

    Added recipie sharing app

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,6 +5,11 @@ const useRecipeStore = create((set) => ({
   addRecipe: (newRecipe) => set((state) => ({recipes: [...state.recipes, newRecipe]})),
   deleteRecipe: (title) => set((state) => ({recipes: state.recipes.filter(recipe => (title !== recipe.title))})),
   getDetailsRecipe: (id) => set((state) => ({recipes: state.recipes.find(recipe => (id === recipe.id))})),
+  updateRecipe: (recipeId, newTitle) => set(state => ({recipes: state.recipes.map((recipe) => {
+    if (recipe.id === recipeId) {
+      return {...recipe, title = newTitle}
+    }
+  })})),
   setRecipes: (recipes) => set({recipes})
 }))
 
