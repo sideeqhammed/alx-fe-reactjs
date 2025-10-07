ba0d99b2dc5cd31f03ef8631c4544386f8509f93
sideeqhammed <sideeqhammed05.com>
2025-10-07 18:04:14 +0100

    Built the Recipe Detail Page

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -20,10 +20,10 @@ function RecipeDetail () {
   }
 
   return (
-    <div className=" mx-auto max-w-2xl xl:max-w-3xl text-center">
+    <div className="max-w-2xl xl:max-w-3xl mx-5 my-5 text-center rounded-lg shadow-lg">
       <Link to='/' className="mx-3 mt-5 block text-start">⬅️ Back to recipes</Link>
       <h2 className="font-bold text-2xl mx-5 my-10 ">Recipe: {recipe.title}</h2>
-      <img className="mx-auto h-20 w-20" src={recipe.image} alt={recipe.title}/>
+      <img className="mx-auto h-40 w-50 rounded-md" src={recipe.image} alt={recipe.title}/>
       <p className="mx-4 my-5">Detail: {recipe.summary}</p>
       <p>ingredients</p>
       <p>instructions</p>
