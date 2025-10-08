ceeacc34ec867ba812b18a7b8100ce7eae0e408a
sideeqhammed <sideeqhammed05.com>
2025-10-08 11:06:54 +0100

    Added a Form to Add New Recipes

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -6,12 +6,12 @@ function AddRecipeForm() {
   const [preparation, setPreparation] = useState('')
   const [errors, setErrors] = useState(false)
 
-  const validate = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
     if (!(title && ingredients && preparation)) {setErrors(true)} else {setErrors(false)}
   }
   return (
-    <form className="text-center" onSubmit={validate}>
+    <form className="text-center" onSubmit={handleSubmit}>
       <input name='title' value={title} type="text" placeholder="Recipe Title" onChange={(e) => setTitle(e.target.value)} className="border-1 m-3 mt-8 p-2"/>
       <textarea name="ingredients" value={ingredients} placeholder="Ingredients" onChange={(e) => setIngredients(e.target.value)} className="border-1 mx-auto my-3 p-2 block" cols={30} rows={5}></textarea>
       <textarea name="preparation_steps" value={preparation} placeholder="Preparation" onChange={(e) => setPreparation(e.target.value)} className="border-1 mx-auto my-3 p-2 block" cols={30} rows={5}></textarea>
