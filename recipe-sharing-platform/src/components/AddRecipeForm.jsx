720eca31b949f4db8a8c937af9d72f1a5aec41e7
sideeqhammed <sideeqhammed05.com>
2025-10-08 11:02:00 +0100

    Added a Form to Add New Recipes

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -14,7 +14,7 @@ function AddRecipeForm() {
     <form className="text-center" onSubmit={handleSubmit}>
       <input name='title' value={title} type="text" placeholder="Recipe Title" onChange={(e) => setTitle(e.target.value)} className="border-1 m-3 mt-8 p-2"/>
       <textarea name="ingredients" value={ingredients} placeholder="Ingredients" onChange={(e) => setIngredients(e.target.value)} className="border-1 mx-auto my-3 p-2 block" cols={30} rows={5}></textarea>
-      <textarea name="preparation" value={preparation} placeholder="Preparation" onChange={(e) => setPreparation(e.target.value)} className="border-1 mx-auto my-3 p-2 block" cols={30} rows={5}></textarea>
+      <textarea name="preparation_steps" value={preparation} placeholder="Preparation" onChange={(e) => setPreparation(e.target.value)} className="border-1 mx-auto my-3 p-2 block" cols={30} rows={5}></textarea>
       {error ? <p className="text-red-600">Recipe form not fully filled, please fill all</p> : ''}
       <button type="submit" className='border-1 px-2 py-1 rounded-xl bg-gray-300 mt-3'>Submit</button>
     </form>
